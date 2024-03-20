9363ae20b043d7ea6d05652039b02215c6a078de
Daniel Sousa <daniel.sousa@elo.com.br>
2024-03-20 15:49:50 -0300

    disabled prefix configuration due SWA

--- next.config.mjs
+++ next.config.mjs
@@ -4,14 +4,16 @@ const nextConfig = {
   // Em desenvolvimento vai ser acessado como http://localhost:3000/public-only
   // no MF main é possível configurar para qualquer outra rota, mas o mais ideal
   // é que ambas tenham a mesma nomeclatura
-  basePath: "/public-only",
+  // ------ disabled due SWA ------
+  // basePath: "/public-only",
 
   // Configura para que os recursos (css, js, etc) sejam baixados da URL de deploy
   // desse MF, caso contrário, o MF vai tentar baixar do MF main, resultado em 404
   //
   // Obs.: Comente a linha abaixo caso esteja desenvolvendo fora de forma extena aos
   // microfrontends (caso opte por desenvolver o app e depois conectar aos MF main)
-  assetPrefix: "/public-only",
+  // assetPrefix: "/public-only",
+  // ------ disabled due SWA ------
 };
 
 export default nextConfig;
