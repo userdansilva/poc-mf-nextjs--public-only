ab9ab9b2e680a92e1f4a14e8066e3c5db55981f9
Daniel Sousa <daniel.sousa@elo.com.br>
2024-03-20 21:56:44 -0300

    try base path with rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -5,14 +5,35 @@ const nextConfig = {
   // no MF main é possível configurar para qualquer outra rota, mas o mais ideal
   // é que ambas tenham a mesma nomeclatura
   // ------ disabled due SWA ------
-  // basePath: "/public-only",
+  basePath: "/public-only",
 
   // Configura para que os recursos (css, js, etc) sejam baixados da URL de deploy
   // desse MF, caso contrário, o MF vai tentar baixar do MF main, resultado em 404
   //
   // Obs.: Comente a linha abaixo caso esteja desenvolvendo fora de forma extena aos
   // microfrontends (caso opte por desenvolver o app e depois conectar aos MF main)
-  assetPrefix: process.env.BASE_URL,
+  assetPrefix: `${process.env.BASE_URL}/public-only`,
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: '/',
+          destination: `${process.env.BASE_URL}/public-only`,
+          basePath: false
+        }
+      ]
+    }
+  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: '/public-only',
+  //       basePath: false,
+  //       permanent: false
+  //     }
+  //   ]
+  // }
   // ------ disabled due SWA ------
 };
 
